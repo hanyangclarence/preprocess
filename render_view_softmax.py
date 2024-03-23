236c57c626595c83be8fb14927a5bf93f7d9d8de
Cralence <mahlerrrr76@gmail.com>
2024-03-23 09:13:24 +0800

    update

--- render_view_softmax.py
+++ render_view_softmax.py
@@ -4,9 +4,7 @@ from splatting import splatting_function
 import torch
 from torchvision.transforms import ToPILImage, ToTensor
 
-def render_forward_splat(src_imgs, src_depths,
-                           r_cam, t_cam, k_src,
-                           k_dst):
+def render_forward_splat(src_imgs, src_depths, R_src, t_src, K_src, R_dst, t_dst, K_dst):
     """3D render the image to the next viewpoint.
 
     Returns:
@@ -16,24 +14,24 @@ def render_forward_splat(src_imgs, src_depths,
     """
     batch_size = src_imgs.shape[0]
 
-    rot = r_cam
-    t = t_cam
-    k_src_inv = k_src.inverse()
+    K_src_inv = K_src.inverse()
+    R_dst_inv = R_dst.inverse()
 
     x = np.arange(src_imgs[0].shape[1])
     y = np.arange(src_imgs[0].shape[0])
     coord = np.stack(np.meshgrid(x, y), -1)
     coord = np.concatenate((coord, np.ones_like(coord)[:, :, [0]]), -1)
     coord = coord.astype(np.float32)
-    coord = torch.as_tensor(coord, dtype=k_src.dtype, device=k_src.device)
+    coord = torch.as_tensor(coord, dtype=K_src.dtype, device=K_src.device)
     coord = coord[None, Ellipsis, None].repeat(batch_size, 1, 1, 1, 1)
 
     depth = src_depths[:, :, :, None, None]
 
     # from reference to target viewpoint
-    pts_3d_ref = depth * k_src_inv[None, None, None, Ellipsis] @ coord
-    pts_3d_tgt = rot[None, None, None, Ellipsis] @ pts_3d_ref + t[None, None, None, :, None]
-    points = k_dst[None, None, None, Ellipsis] @ pts_3d_tgt
+    pts_3d_ref = depth * K_src_inv[None, None, None, Ellipsis] @ coord
+    pts_3d_tgt = R_dst_inv[None, None, None, Ellipsis] @ R_src[None, None, None, Ellipsis] @ pts_3d_ref + \
+                    R_dst_inv[None, None, None, Ellipsis] @ (t_src[None, None, None, :, None] - t_dst[None, None, None, :, None])
+    points = K_dst[None, None, None, Ellipsis] @ pts_3d_tgt
     points = points.squeeze(-1)
 
     new_z = points[:, :, :, [2]].clone().permute(0, 3, 1, 2)  # b,1,h,w
@@ -88,9 +86,9 @@ R_source = torch.eye(3)
 t_source = torch.tensor([0.0, 0.0, 0.0])
 
 R_target = torch.eye(3)
-t_target = torch.tensor([-0.5, 0.5, 1.0])
+t_target = torch.tensor([0.3, -0.5, -1.0])
 
-warp_feature, warp_disp, warp_mask = render_forward_splat(img, depth_map[None, ...], R_target, t_target, K, K)
+warp_feature, warp_disp, warp_mask = render_forward_splat(img, depth_map[None, ...], R_source, t_source, K, R_target, t_target, K)
 feature = ToPILImage()(warp_feature[0])
 disp = ToPILImage()(warp_disp[0])
 mask = ToPILImage()(warp_mask[0])
